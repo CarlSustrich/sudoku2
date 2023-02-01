7ef3c8710d9fe0182ff92adb0565d8966fdbda0e
osbrown1 <socknot@live.com>
2023-02-01 13:38:51 -0800

    Co-authored-by: jacksonlevine <jacksonlevine@users.noreply.github.com> Co-authored-by: Carl Sustrich <CarlSustrich@users.noreply.github.com>

--- __tests__/grid.test.js
+++ __tests__/grid.test.js
@@ -1,6 +1,6 @@
 import Grid from './../src/grid.js'
 
-describe ('Grid', () => {
+describe ('Grid object functionality', () => {
 
   let newGrid;
   beforeEach(() => {
@@ -32,10 +32,100 @@ describe ('Grid', () => {
     expect(newGrid.row9).toEqual([0, 5, 0, 3, 0, 0, 8, 9, 0]);
   });
 
-  test('it should return true if a row contains the numbers 1-9 and no repeats ', () => {
+  test('it should return true if a row contains no repeats', () => {
     newGrid.row1 = [6, 2, 3, 7, 1, 8, 5, 4, 9];
     expect(newGrid.checkRow(1)).toEqual(true);
   });
 
-})
+  test('it should return false if a row contains any repeats', () => {
+    newGrid.row1 = [8, 8, 2, 3, 4, 5, 6, 7, 8];
+    expect(newGrid.checkRow(1)).toEqual(false);
+  });
+
+  test('it should return true if a column contains no repeats', () => {
+    newGrid.row1 = [1];
+    newGrid.row2 = [2];
+    newGrid.row3 = [3];
+    newGrid.row4 = [4];
+    newGrid.row5 = [5];
+    newGrid.row6 = [6];
+    newGrid.row7 = [7];
+    newGrid.row8 = [8];
+    newGrid.row9 = [9];
+    expect(newGrid.checkColumn(1)).toEqual(true);
+  });
+
+  test('it should return false if a column has any repeats', () => {
+    newGrid.row1 = [2];
+    newGrid.row2 = [2];
+    newGrid.row3 = [3];
+    newGrid.row4 = [4];
+    newGrid.row5 = [5];
+    newGrid.row6 = [6];
+    newGrid.row7 = [7];
+    newGrid.row8 = [8];
+    newGrid.row9 = [9];
+    expect(newGrid.checkColumn(1)).toEqual(false);
+  });
+
+  test('it should return true if a 3x3 square has no repeats, or false if it does', () => {
+    newGrid.row1 = [6, 8, 7, 7, 0, 0, 0, 5, 4];
+    newGrid.row2 = [9, 5, 3, 0, 0, 0, 6, 0, 7];
+    newGrid.row3 = [4, 2, 1, 8, 6, 0, 9, 0, 3];
+    newGrid.row4 = [1, 3, 0, 0, 8, 0, 4, 0, 0];
+    newGrid.row5 = [0, 6, 0, 0, 1, 0, 0, 0, 8];
+    newGrid.row6 = [0, 0, 0, 0, 3, 0, 0, 6, 0];
+    newGrid.row7 = [0, 0, 0, 6, 0, 9, 5, 4, 7];
+    newGrid.row8 = [0, 0, 9, 0, 0, 0, 6, 2, 3];
+    newGrid.row9 = [0, 5, 0, 3, 0, 0, 8, 9, 1];
+    expect(newGrid.checkSquare(1)).toEqual(true);
+    expect(newGrid.checkSquare(2)).toEqual(false);
+    expect(newGrid.checkSquare(4)).toEqual(false);
+    expect(newGrid.checkSquare(9)).toEqual(true);
+  });
+
+  test('it should return true if every row, every column, and every square returns true upon being checked', () => {
+    newGrid.row1 = [6, 8, 7, 7, 0, 0, 0, 5, 4];
+    newGrid.row2 = [9, 5, 3, 0, 0, 0, 6, 0, 7];
+    newGrid.row3 = [4, 2, 1, 8, 6, 0, 9, 0, 3];
+    newGrid.row4 = [1, 3, 0, 0, 8, 0, 4, 0, 0];
+    newGrid.row5 = [0, 6, 0, 0, 1, 0, 0, 0, 8];
+    newGrid.row6 = [0, 0, 0, 0, 3, 0, 0, 6, 0];
+    newGrid.row7 = [0, 0, 0, 6, 0, 9, 5, 4, 7];
+    newGrid.row8 = [0, 0, 9, 0, 0, 0, 6, 2, 3];
+    newGrid.row9 = [0, 5, 0, 3, 0, 0, 8, 9, 1];
+    expect(newGrid.completeCheck()).toEqual(false);
+    newGrid.row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row4 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row7 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row8 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(newGrid.completeCheck()).toEqual(false);
+    newGrid.row1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    newGrid.row2 = [2, 3, 4, 5, 6, 7, 8, 9, 1];
+    newGrid.row3 = [3, 4, 5, 6, 7, 8, 9, 1, 2];
+    newGrid.row4 = [4, 5, 6, 7, 8, 9, 1, 2, 3];
+    newGrid.row5 = [5, 6, 7, 8, 9, 1, 2, 3, 4];
+    newGrid.row6 = [6, 7, 8, 9, 1, 2, 3, 4, 5];
+    newGrid.row7 = [7, 8, 9, 1, 2, 3, 4, 5, 6];
+    newGrid.row8 = [8, 9, 1, 2, 3, 4, 5, 6, 7];
+    newGrid.row9 = [9, 1, 2, 3, 4, 5, 6, 7, 8];
+    expect(newGrid.completeCheck()).toEqual(false);
+    newGrid.row1 = [1, 5, 2, 4, 6, 9, 3, 7, 8];
+    newGrid.row2 = [7, 8, 9, 2, 1, 3, 4, 5, 6];
+    newGrid.row3 = [4, 3, 6, 5, 8, 7, 2, 9, 1];
+    newGrid.row4 = [6, 1, 3, 8, 7, 2, 5, 4, 9];
+    newGrid.row5 = [9, 7, 4, 1, 5, 6, 8, 2, 3];
+    newGrid.row6 = [8, 2, 5, 9, 3, 4, 1, 6, 7];
+    newGrid.row7 = [5, 6, 7, 3, 4, 8, 9, 1, 2];
+    newGrid.row8 = [2, 4, 8, 6, 9, 1, 7, 3, 5];
+    newGrid.row9 = [3, 9, 1, 7, 2, 5, 6, 8, 4];
+    expect(newGrid.completeCheck()).toEqual(true);
+  })
+
+});
 
--- src/grid.js
+++ src/grid.js
@@ -1,29 +1,74 @@
-export default function Grid () {
-  this.row1 = [];
-  this.row2 = [];
-  this.row3 = [];
-  this.row4 = [];
-  this.row5 = [];
-  this.row6 = [];
-  this.row7 = [];
-  this.row8 = [];
-  this.row9 = [];
-}
+export default class Grid {
 
-Grid.prototype.initialize = function() {
-  this.row1 = [6, 0, 0, 7, 0, 0, 0, 5, 4];
-  this.row2 = [9, 0, 0, 0, 0, 0, 6, 0, 7];
-  this.row3 = [4, 2, 0, 8, 6, 0, 9, 0, 3];
-  this.row4 = [1, 3, 0, 0, 8, 0, 4, 0, 0];
-  this.row5 = [0, 6, 0, 0, 1, 0, 0, 0, 8];
-  this.row6 = [0, 0, 0, 0, 3, 0, 0, 6, 0];
-  this.row7 = [0, 0, 0, 6, 0, 9, 0, 4, 0];
-  this.row8 = [0, 0, 9, 0, 0, 0, 0, 0, 0];
-  this.row9 = [0, 5, 0, 3, 0, 0, 8, 9, 0];
-}
+  constructor(){
+    this.row1 = [];
+    this.row2 = [];
+    this.row3 = [];
+    this.row4 = [];
+    this.row5 = [];
+    this.row6 = [];
+    this.row7 = [];
+    this.row8 = [];
+    this.row9 = [];
+  }
 
-Grid.prototype.checkRow = function(rowNumber) {
-  let numbersInArray = this[`row${rowNumber}`];
-  const rowSet = new Set(numbersInArray);
-  return numbersInArray.length === rowSet.size;
+  initialize = function() {
+    this.row1 = [6, 0, 0, 7, 0, 0, 0, 5, 4];
+    this.row2 = [9, 0, 0, 0, 0, 0, 6, 0, 7];
+    this.row3 = [4, 2, 0, 8, 6, 0, 9, 0, 3];
+    this.row4 = [1, 3, 0, 0, 8, 0, 4, 0, 0];
+    this.row5 = [0, 6, 0, 0, 1, 0, 0, 0, 8];
+    this.row6 = [0, 0, 0, 0, 3, 0, 0, 6, 0];
+    this.row7 = [0, 0, 0, 6, 0, 9, 0, 4, 0];
+    this.row8 = [0, 0, 9, 0, 0, 0, 0, 0, 0];
+    this.row9 = [0, 5, 0, 3, 0, 0, 8, 9, 0];
+  };
+
+  checkRow = function(rowNumber) {
+    let numbersInArray = this[`row${rowNumber}`];
+    const numbersInSet = new Set(numbersInArray);
+    return (numbersInArray.length === numbersInSet.size);
+  };
+
+  checkColumn = function(columnNumber) {
+    let numbersInArray = [];
+    for(let i = 0; i < 9; i+=1) {
+      numbersInArray.push(this[`row${i+1}`][columnNumber-1]);
+    }
+    const numbersInSet = new Set(numbersInArray);
+    return (numbersInArray.length === numbersInSet.size);
+  };
+
+  checkSquare = function(squareNumber) {
+    let numbersInArray = [];
+    for(let i = 0; i < 3; i+=1) {
+      for(let j = 0; j < 3; j+=1) {
+        let rowOffset = Math.floor( (squareNumber-1)/3 ) ;    
+        let rowNumber = i + 1 + rowOffset;
+        let indexInRow = (((squareNumber-1)%3)*3) + j;
+        numbersInArray.push(this[`row${rowNumber}`][indexInRow]);
+      }
+    }
+    const numbersInSet = new Set(numbersInArray);
+    return (numbersInArray.length === numbersInSet.size);
+  };
+
+  completeCheck = function() {
+    for(let i = 0; i < 9; i++) {
+      if(!this.checkRow(i+1)) {
+        return false;
+      }
+    }
+    for(let i = 0; i < 9; i++) {
+      if(!this.checkColumn(i+1)) {
+        return false;
+      }
+    }
+    for(let i = 0; i < 9; i++) {
+      if(!this.checkSquare(i+1)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
